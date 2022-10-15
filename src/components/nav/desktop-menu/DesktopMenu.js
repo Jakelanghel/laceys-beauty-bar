278c1611f6cd1f8d52ac7d1f30b67d4a164c825b
Jake <jacobtlanghel@gmail.com>
2022-10-14 22:13:39 -0400

    fix nav

--- src/components/nav/desktop-menu/DesktopMenu.js
+++ src/components/nav/desktop-menu/DesktopMenu.js
@@ -1,15 +1,71 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { StyledDesktopMenu } from "./DesktopMenu.Styled";
-import Menu from "../Menu";
-import MenuMotion from "../../motion/MenuMotion";
 
 const DesktopMenu = () => {
   return (
-    <MenuMotion>
-      <StyledDesktopMenu>
-        <Menu />
-      </StyledDesktopMenu>
-    </MenuMotion>
+    <StyledDesktopMenu>
+      <div className="link">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Home
+        </NavLink>
+      </div>
+
+      <div className="link">
+        <NavLink
+          to="/lashes"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Lashes
+        </NavLink>
+      </div>
+
+      <div className="link">
+        <NavLink
+          to="/Brows"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Eyebrows
+        </NavLink>
+      </div>
+
+      <div className="link">
+        <NavLink
+          to="/Skin"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Skin
+        </NavLink>
+      </div>
+
+      <div className="link">
+        <NavLink
+          to="/waxing-tanning"
+          className={({ isActive }) =>
+            isActive ? "active nav-link" : "nav-link"
+          }
+        >
+          Waxing/Tanning
+        </NavLink>
+      </div>
+
+      <div className="link">
+        <a href="https://tinyurl.com/2p8vkjtm" className="nav-link">
+          Clothing
+        </a>
+      </div>
+    </StyledDesktopMenu>
   );
 };
 
--- src/components/nav/mobile-menu/MobileMenu.js
+++ src/components/nav/mobile-menu/MobileMenu.js
@@ -1,16 +1,112 @@
 import React from "react";
 import { StyledMobileMenu } from "./MobileMenu.Styled";
 import { NavLink } from "react-router-dom";
-import MenuMotion from "../../motion/MenuMotion";
-import Menu from "../Menu";
+import { motion } from "framer-motion";
 
 const MobileMenu = (props) => {
+  const menuVariants = {
+    hidden: {
+      position: "fixed",
+      zIndex: 2,
+      y: "-100vh",
+      width: "100vw",
+      height: "100vh",
+      transition: {
+        type: "spring",
+        damping: 20,
+        ease: "easeInOut",
+      },
+    },
+    visible: {
+      y: 0,
+      width: "100vw",
+      height: "100vh",
+      transition: {
+        type: "spring",
+        damping: 20,
+        ease: "easeInOut",
+      },
+    },
+    exit: {
+      x: "-100vw",
+      transition: {
+        type: "spring",
+        damping: 20,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
-    <MenuMotion>
+    <motion.div
+      variants={menuVariants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
       <StyledMobileMenu>
-        <Menu />
+        <div className="link">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "active nav-link" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+        </div>
+
+        <div className="link">
+          <NavLink
+            to="/lashes"
+            className={({ isActive }) =>
+              isActive ? "active nav-link" : "nav-link"
+            }
+          >
+            Lashes
+          </NavLink>
+        </div>
+
+        <div className="link">
+          <NavLink
+            to="/Brows"
+            className={({ isActive }) =>
+              isActive ? "active nav-link" : "nav-link"
+            }
+          >
+            Eyebrows
+          </NavLink>
+        </div>
+
+        <div className="link">
+          <NavLink
+            to="/Skin"
+            className={({ isActive }) =>
+              isActive ? "active nav-link" : "nav-link"
+            }
+          >
+            Skin
+          </NavLink>
+        </div>
+
+        <div className="link">
+          <NavLink
+            to="/waxing-tanning"
+            className={({ isActive }) =>
+              isActive ? "active nav-link" : "nav-link"
+            }
+          >
+            Waxing/Tanning
+          </NavLink>
+        </div>
+
+        <div className="link">
+          <a href="https://tinyurl.com/2p8vkjtm" className="nav-link">
+            Clothing
+          </a>
+        </div>
       </StyledMobileMenu>
-    </MenuMotion>
+    </motion.div>
   );
 };
 
